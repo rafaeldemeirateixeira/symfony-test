e2793247978e08954d91592568534be538ff2498
Rafael Teixeira <rafaeldemeirateixeira@gmail.com>
2021-10-27 09:41:45 -0300

    [Refactor] - Adjusted the limit of requests per minute - Added method for generating keys

--- src/Service/CreateHashService.php
+++ src/Service/CreateHashService.php
@@ -33,27 +33,41 @@ class CreateHashService
         $subDate = $now->sub(new DateInterval('PT1M'))->format('Y-m-d H:i:s');
         $totalLastMinute = $this->hashesRepository->getLastMinuteByIp($subDate, $ip);
 
-        if ($totalLastMinute >= 2) {
+        if ($totalLastMinute >= 10) {
             throw new TooManyRequestsHttpException(60, 'Too Many Attempts.');
         }
 
         //TODO: Gerar gerar key
+        $key = $this->keyGenerate();
+        $md5 = md5(u($data['input'])->append($key));
         
         $hash = [
             'input' => $data['input'],
-            'hash' => u('0000')->append(
-                md5(
-                    u($data['input'])->append(
-                        mt_rand(10000000, 99999999)
-                    )
-                )
-            ),
-            'key' => '22334455',
+            'hash' => u('0000')->append($md5),
+            'key' => $key,
             'block_number' => $data['block_number'],
             'attempts' => 12,
             'ip' => $ip
         ];
 
         return $this->hashesRepository->saveHash($hash);
+    }
+
+    /**
+     * @param mixed $input
+     * @param mixed $strength
+     * @return string
+     */
+    public function keyGenerate(int $strength = 8)
+    {
+        $input = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $inputLength = strlen($input);
+        $randomString = '';
+        for ($i = 0; $i < $strength; $i++) {
+            $randomCharacter = $input[mt_rand(0, $inputLength - 1)];
+            $randomString .= $randomCharacter;
+        }
+
+        return $randomString;
     }
 }
